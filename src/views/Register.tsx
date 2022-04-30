750d873bdda634075c609e49d7db044e32324661
Lucas Surdi Franco <surdifranco@gmail.com>
2022-04-30 12:10:28 -0300

    feat: changing register and login views
    Added translation hook.
    Added validation errors support.

--- src/views/Register.tsx
+++ src/views/Register.tsx
@@ -1,8 +1,7 @@
 import React, { useContext } from 'react'
-import { Link as RouterLink } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
 import { RegisterContext } from '../contexts/register'
-import { i18n } from '../i18n'
 import { routes } from './routes'
 
 import { Button } from '../components/Button'
@@ -11,38 +10,63 @@ import { Link } from '../components/Link'
 import { TextField } from '../components/TextField'
 
 const Register: React.FC = () => {
+  const { t } = useTranslation(['register'])
   const register = useContext(RegisterContext)
+
+  const credentials = register.credentials
+  const loading = register.loading
 
   const handleChange = register.handleChange
   const signUp = register.signUp
 
   return (
-    <div className="[ register ] w-full h-full flex items-center justify-center">
+    <main className="[ register ] w-full h-full flex items-center justify-center">
       <div className="p-4 w-full">
-        <h3 className="font-bold text-gray-800 text-center mb-2">{ i18n.t('register:title') }</h3>
-        <h6 className="text-gray-500 text-center mb-16">{ i18n.t('register:subtitle') }</h6>
+        <h3 className="font-bold text-gray-800 text-center mb-2">{ t('register:title') }</h3>
+        <h6 className="text-gray-500 text-center mb-16">{ t('register:subtitle') }</h6>
         <div className="grid gap-4">
-          <Field label={i18n.t('register:username')}>
-            <TextField key="username" _onChange={handleChange} />
+          <Field label={t('register:username')} error={credentials.username.error?.message}>
+            <TextField
+              name="username"
+              icon="user"
+              error={!!credentials.username.error}
+              onChange={handleChange}
+            />
           </Field>
-          <Field label={i18n.t('register:email')}>
-            <TextField key="email" _onChange={handleChange} />
+          <Field label={t('register:email')} error={credentials.email.error?.message}>
+            <TextField
+              name="email"
+              icon="envelope"
+              error={!!credentials.email.error}
+              onChange={handleChange}
+            />
           </Field>
-          <Field label={i18n.t('register:password')}>
-            <TextField key="password" type="password" _onChange={handleChange} />
+          <Field label={t('register:password')} error={credentials.password.error?.message}>
+            <TextField
+              name="password"
+              icon="lock"
+              type="password"
+              error={!!credentials.password.error}
+              onChange={handleChange}
+            />
           </Field>
-          <Field label={i18n.t('register:confirm-password')}>
-            <TextField key="confirmPassword" type="password" _onChange={handleChange} />
+          <Field label={t('register:confirm-password')} error={credentials.confirmPassword.error?.message}>
+            <TextField
+              name="confirmPassword"
+              type="password"
+              error={!!credentials.confirmPassword.error}
+              onChange={handleChange}
+            />
           </Field>
           <div className="grid gap-3">
-            <Button onClick={signUp}>{ i18n.t('register:register-btn') }</Button>
-            <RouterLink className="text-sm text-center" to={routes.login}>
-              <Link className="ml-1">{ i18n.t('register:to-login') }</Link>
-            </RouterLink>
+            <Button loading={loading.signUp} onClick={signUp}>{ t('register:register-btn') }</Button>
+            <Link className="text-sm text-center" to={routes.login}>
+              { t('register:to-login') }
+            </Link>
           </div>
         </div>
       </div>
-    </div>
+    </main>
   )
 }
 
